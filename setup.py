cfe83ab7dd1c74c3e83537882b1d76fad55b1052
Omegastick <isaac@poulton.dev>
2022-03-04 14:19:12 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="probability_tree",
+    version="0.1",
+    description="A small library for building probability trees and calculating their results",
+    author="Isaac Poulton",
+    author_email="isaac@poulton.dev",
+    url="https://github.com/Omegastick/probability-tree",
+    packages=["probability_tree"],
+)
